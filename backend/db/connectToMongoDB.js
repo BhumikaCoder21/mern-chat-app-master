d4a005a60c7f2807321ad870c758370b79c3f13c
BhumikaCoder21 <bhumikanew21@gmail.com>
2025-02-09 02:32:37 +0530

    fix

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,25 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+  try {
+    if (!process.env.MONGO_DB_URI) {
+      throw new Error(
+        "MONGO_DB_URI is not defined in the environment variables"
+      );
+    }
+
+    await mongoose.connect(
+      process.env.MONGO_DB_URI,
+
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
 };
 
 export default connectToMongoDB;
