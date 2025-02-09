f069164766aaff03aee5b50ebc23b5a6540ae82b
BhumikaCoder21 <bhumikanew21@gmail.com>
2025-02-09 21:56:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import cors from "cors";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
@@ -18,6 +19,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,  // Frontend URL (Vercel domain)
+    credentials: true,
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
