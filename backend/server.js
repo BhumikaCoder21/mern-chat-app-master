75ed43b704884b76f0c69e49db7ffb1f9ea504a0
BhumikaCoder21 <bhumikanew21@gmail.com>
2025-02-09 21:53:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,19 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
 server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 }).on("error", (err) => {
 	if (err.code === "EADDRINUSE") {
 		console.error(`Port ${PORT} is already in use. Trying a different port...`);
-		const newPort = Math.floor(Math.random() * 1000) + 4000; // Random port between 4000-5000
+		const newPort = Math.floor(Math.random() * 1000) + 4000;
 		server.listen(newPort, () => console.log(`Server running on port ${newPort}`));
 	}
 });
