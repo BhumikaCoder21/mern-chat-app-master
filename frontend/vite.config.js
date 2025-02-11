880ad48fa025467263e4b408999b773dca3cca0f
Bhumika Gupta <bhumikanew21@gmail.com>
2025-02-11 16:31:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import process from 'node:process';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
+export default defineConfig(({ mode }) => {
+	// Load environment variables from the client/.env file
+	const env = loadEnv(mode, process.cwd() + "/frontend");
+
+	return {
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: env.VITE_API_URL,
+					changeOrigin: true,
+					secure: false,
+				},
 			},
 		},
-	},
+	};
 });
