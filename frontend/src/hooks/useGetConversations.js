630141a23656754c456db14fd54c6ff9f93b839e
Bhumika Gupta <bhumikanew21@gmail.com>
2025-02-11 16:42:09 +0530

    Update useGetConversations.js

--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -4,12 +4,13 @@ import toast from "react-hot-toast";
 const useGetConversations = () => {
 	const [loading, setLoading] = useState(false);
 	const [conversations, setConversations] = useState([]);
+	const BASE_URL = import.meta.env.VITE_API_URL;
 
 	useEffect(() => {
 		const getConversations = async () => {
 			setLoading(true);
 			try {
-				const res = await fetch("/api/users");
+				const res = await fetch(`${BASE_URL}/api/users`);
 				const data = await res.json();
 				if (data.error) {
 					throw new Error(data.error);
@@ -23,7 +24,7 @@ const useGetConversations = () => {
 		};
 
 		getConversations();
-	}, []);
+	}, [BASE_URL]);
 
 	return { loading, conversations };
 };
